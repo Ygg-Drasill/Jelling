3ab90312067e02bad84d3ad69ffc02e4da82302b
alexrefshauge <nk02hb@aau.student.dk>
2025-03-25 22:15:02 +0100

    initial bubble tea

---
+++ cli/jell/cmd/root.go
@@ -0,0 +1,83 @@
+/*
+Copyright © 2025 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/spf13/cobra"
+)
+
+type model struct {
+	x int
+}
+
+func (m model) Init() tea.Cmd {
+	return nil
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	var cmd tea.Cmd
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "q", "ctrl+c":
+			cmd = tea.Quit
+			break
+		case "up":
+			m.x++
+			break
+		case "down":
+			m.x--
+			break
+		}
+
+	}
+	return m, cmd
+}
+
+func (m model) View() string {
+	s := fmt.Sprintf("Hello World %d", m.x)
+	return s
+}
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "jell",
+	Short: "A brief description of your application",
+	Long: `A longer description that spans multiple lines and likely contains
+examples and usage of using your application. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	// Uncomment the following line if your bare application
+	// has an action associated with it:
+	Run: func(cmd *cobra.Command, args []string) {
+		tea.NewProgram(model{x: 1}).Run()
+	},
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	// Here you will define your flags and configuration settings.
+	// Cobra supports persistent flags, which, if defined here,
+	// will be global for your application.
+
+	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.jell.yaml)")
+
+	// Cobra also supports local flags, which will only run
+	// when this action is called directly.
+	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
---
+++ cli/jell/main.go
@@ -0,0 +1,10 @@
+/*
+Copyright © 2025 Ygg-Drasill tobiasabay@gmail.com
+*/
+package main
+
+import "github.com/Ygg-Drasill/Jelling/cli/jell/cmd"
+
+func main() {
+	cmd.Execute()
+}
